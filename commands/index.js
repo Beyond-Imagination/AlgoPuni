9e33116b22b0183fcc59dfcf2516464979922d6e
구원 라 <laggu91@gmail.com>
2021-02-10 09:00:17 +0000

    set version to v0.1.0

--- commands/index.js
+++
@@ -1,9 +0,0 @@
-import init from './init';
-import user from './user';
-import problem from './problem';
-
-export default {
-    'init': init,
-    'user': user,
-    'problem': problem,
-}
--- params.js
+++ params.js
@@ -1,4 +1,4 @@
-export const VERSION = '0.0.1';
+export const VERSION = '0.1.0';
 export const ALGOPUNIDIR = '.algopuni';
 export const USERJSON = `${ALGOPUNIDIR}/user.json`;
 export const DATAJSON = `${ALGOPUNIDIR}/data.json`;
