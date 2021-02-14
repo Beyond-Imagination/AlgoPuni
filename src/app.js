b2b876c61761331bd23a6769792b8916896590e2
경현 이 <xoelxowl92@gmail.com>
2021-02-14 12:11:16 +0000

    allocate yarn start's default command  to help

--- src/app.js
+++ src/app.js
@@ -10,11 +10,15 @@ program.version(VERSION);
 program.on('option:debug', function () {
     log.setLevel("debug")
 });
-
 program.addOption(new Option('-d, --debug').hideHelp())
+
+program.action(() => {
+    program.help();
+});
 
 program.addCommand(init);
 program.command('user', "user command description", { executableFile: './commands/user/index' })
 program.command('problem', "problem command description", { executableFile: './commands/problem/index' })
+
 
 export default program;
