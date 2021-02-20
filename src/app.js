5f8391019dcea9feda8c19922e2c1511996bf9af
구원 라 <laggu91@gmail.com>
2021-02-20 09:07:51 +0000

    set version to v0.2.0

--- src/app.js
+++ src/app.js
@@ -1,20 +1,15 @@
 import { Command, Option } from 'commander'
 import log from './utils/log'
+import {version} from '../package.json';
 
-import { VERSION } from './params'
 import init from'./commands/init'
 
 const program = new Command();
-program.version(VERSION);
+program.version(version);
 
-program.on('option:debug', function () {
-    log.setLevel("debug")
-});
+program.on('option:debug', () => log.setLevel("debug"));
 program.addOption(new Option('-d, --debug').hideHelp())
-
-program.action(() => {
-    program.help();
-});
+program.action(() => program.help());
 
 program.addCommand(init);
 program.command('user', "user command description", { executableFile: './commands/user/index' })
--- src/params.js
+++ src/params.js
@@ -1,5 +1,3 @@
-export const VERSION = '0.1.0';
-
 export const ALGOPUNIDIR = '.algopuni';
 export const USERJSON = `${ALGOPUNIDIR}/user.json`;
 export const DATAJSON = `${ALGOPUNIDIR}/data.json`;
